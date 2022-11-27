fd492c6da660919727d8361b366c074626994395
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-11-27 17:24:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
+     int numero;
+    int primoNumero=1;
+    int secondoNumero=1;
+    cout<<"inserire il numero di cifre della serie di fibonacci da calcolare";
+    cin>>numero;
+    cout<< primoNumero<<endl<<secondoNumero <<endl ;
+        int i = 3;
+        while (i++ <= numero && primoNumero+secondoNumero<=numero) {
+            cout << primoNumero + secondoNumero <<endl;
+            if (primoNumero < secondoNumero)
+                primoNumero += secondoNumero;
+
+            else
+                secondoNumero += primoNumero;
+        }
+
+    
    return 0;
 }
